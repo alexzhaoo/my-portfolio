84c76ffadd50b0c131c27f4a279de7a63476c297
Alexander Zhao <zhaotianxiao722@icloud.com>
2024-01-06 22:39:43 -0500

    new resume

--- app/page.js
+++ app/page.js
@@ -133,7 +133,7 @@ function Home() {
       <a>Projects</a>
       </li>
       <li className='mx-5 hover:-translate-y-1'>
-        <a className='bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 rounded-md' href="https://drive.google.com/file/d/1_jNZNPKrMCj4XdgVHAviRhQO3RX8lrPm/view?usp=sharing" target='_blank' rel="noopener noreferrer" >Resume</a>
+        <a className='bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 rounded-md' href="https://drive.google.com/file/d/1m35w2yJsg5rqr18XIKSdYJOc_We6TOgS/view?usp=sharing" target='_blank' rel="noopener noreferrer" >Resume</a>
       </li> 
     </ul>
       </nav>
